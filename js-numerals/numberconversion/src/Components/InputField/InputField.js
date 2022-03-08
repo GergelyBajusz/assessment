bc49848cc4937c1f06b567eebc2afd7a6c29d4ef
Gergely Bajusz <gergely.bajusz@gmail.com>
2022-03-08 16:04:48 +0100

    new component for individual textfield

---
+++ js-numerals/numberconversion/src/Components/InputField/InputField.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import PropTypes from "prop-types";
+import { TextField, Typography, Card, CardContent } from '@mui/material'
+import { SecondaryColorLight, PrimaryWhite } from '../../Constants/colors'
+
+import './styles.scss'
+
+
+function InputField({heading, type, value, label, handleChange, disabled}) {
+  return (
+          <Card
+           className='InputField'
+           sx={{
+            minWidth: 400,
+            minHeight: 400,
+            margin: '4px',
+            textAlign: 'center',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: SecondaryColorLight,
+            color: PrimaryWhite,
+            '@media (max-width: 990px)': {
+              minWidth: 300,
+              minHeight: 300,
+            },
+          }}
+          >
+            {heading &&
+            <CardContent>
+                <Typography variant="h5" component="div">
+                    {heading}
+                 </Typography>
+            </CardContent>}
+            <TextField
+              multiline
+              type={type}
+              label={label}
+              variant="outlined"
+              value={value}
+              onChange={handleChange}
+              disabled={disabled}
+            />
+          </Card>
+  );
+}
+
+InputField.propTypes = {
+    heading: PropTypes.string,
+    label: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+    handleChange: PropTypes.func,
+    disabled: PropTypes.bool,
+  };
+  
+  InputField.defaultProps = {
+    heading: undefined,
+    handleChange: undefined,
+  };
+
+export default InputField
