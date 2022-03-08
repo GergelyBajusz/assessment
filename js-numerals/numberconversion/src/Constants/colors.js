51b2025741081ce987afd7b864fa8c3f821a390c
Gergely Bajusz <gergely.bajusz@gmail.com>
2022-03-08 16:00:38 +0100

    picked up some color variables

---
+++ js-numerals/numberconversion/src/Constants/colors.js
@@ -0,0 +1,19 @@
+export const PrimaryColor= '#5a1eff';
+export const PrimaryColorLight= '#a584ff';
+export const PrimaryColorDark= '#1d006b';
+
+export const SecondaryColor= '#008099';
+export const SecondaryColorLight= '#22a3bd';
+export const SecondaryColorDark= '#005676';
+export const SecondaryColor20= '#e1f7ff';
+
+export const PrimaryBlack= '#000';
+export const PrimaryBlack80= '#323232';
+
+export const PrimaryGray= '#595959';
+export const PrimaryGray80= '#8b8a87';
+export const PrimaryGray60= '#aba7a5';
+export const PrimaryGray40= '#c5c4c3';
+export const PrimaryGray20= '#e7e7e7';
+
+export const PrimaryWhite= '#fff';
